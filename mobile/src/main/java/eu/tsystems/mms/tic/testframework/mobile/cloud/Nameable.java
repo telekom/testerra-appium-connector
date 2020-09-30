7131f48756e30eb9cfda215c1b8ef1c32bd5303b
Eric Kubenka <Eric.Kubenka@t-systems.com>
2020-09-30 07:37:54 +0200

    Removed old code.

--- mobile/src/main/java/eu/tsystems/mms/tic/testframework/mobile/cloud/Nameable.java
+++
@@ -1,8 +0,0 @@
-package eu.tsystems.mms.tic.testframework.mobile.cloud;
-
-/**
- * Created by rnhb on 09.12.2016.
- */
-public interface Nameable {
-    String name();
-}
--- mobile/src/main/java/eu/tsystems/mms/tic/testframework/mobile/pageobjects/guielement/strategies/MobileGuiElementStrategy.java
+++
@@ -1,9 +0,0 @@
-package eu.tsystems.mms.tic.testframework.mobile.pageobjects.guielement.strategies;
-
-import eu.tsystems.mms.tic.testframework.mobile.pageobjects.guielement.MobileGuiElement;
-
-/**
- * Created by rnhb on 05.01.2016.
- */
-public interface MobileGuiElementStrategy extends MobileGuiElement {
-}
