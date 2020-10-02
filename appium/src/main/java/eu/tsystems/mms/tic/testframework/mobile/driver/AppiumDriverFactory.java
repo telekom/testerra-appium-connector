67cab7f6753f6aa4cb7eaf6d2bcd6d043938d22b
Eric Kubenka <Eric.Kubenka@t-systems.com>
2020-10-02 08:17:08 +0200

    Little fix for cleanups, driver will nwo call getbase url on startup.

--- appium/src/main/java/eu/tsystems/mms/tic/testframework/mobile/driver/AppiumDriverFactory.java
+++ appium/src/main/java/eu/tsystems/mms/tic/testframework/mobile/driver/AppiumDriverFactory.java
@@ -27,6 +27,7 @@ import eu.tsystems.mms.tic.testframework.exceptions.TesterraRuntimeException;
 import eu.tsystems.mms.tic.testframework.exceptions.TesterraSystemException;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.UnspecificWebDriverRequest;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverFactory;
+import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverRequest;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -101,6 +102,7 @@ public class AppiumDriverFactory extends WebDriverFactory<AppiumDriverRequest> {
                     final IOSDriver<IOSElement> driver = new IOSDriver<>(new URL(GRID_URL), desiredCapabilities);
                     final AppiumDeviceQuery appiumDeviceQuery = new AppiumDeviceQuery(driver.getCapabilities());
                     log().info("iOS Session created for: " + appiumDeviceQuery.toString());
+                    driver.get(WebDriverManager.getBaseURL());
                     return driver;
                 } catch (MalformedURLException e) {
                     throw new SessionNotCreatedException("Could not create session, because URL invalid");
@@ -115,6 +117,7 @@ public class AppiumDriverFactory extends WebDriverFactory<AppiumDriverRequest> {
                     final AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL(GRID_URL), desiredCapabilities);
                     final AppiumDeviceQuery appiumDeviceQuery = new AppiumDeviceQuery(driver.getCapabilities());
                     log().info("Android Session created for: " + appiumDeviceQuery.toString());
+                    driver.get(WebDriverManager.getBaseURL());
                     return driver;
                 } catch (MalformedURLException e) {
                     throw new SessionNotCreatedException("Could not create session, because URL invalid");
